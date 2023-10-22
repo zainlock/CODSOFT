957fa72d4c1fdcca5d5d77173bd1793dd1a652aa
zainlock <147399187+zainlock@users.noreply.github.com>
2023-10-22 14:21:58 +0530

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,29 +1,22 @@
+ import string
 import random
-latter=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
-        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-number=['1','2','3','4','5','6','7','8','9','0']
-special_character=['@','#','!','$','%','^','&','*','(',')']
-
-#take input from user
-size_of_Character=int(input("Enter a number of character you want: "))
-size_of_number=int(input("Enter a numbers of number you want: "))
-size_of_special=int(input("Enter a number of special character do you want: "))
-
-password=""
-strong_password=""
-
-for char in range(1,size_of_Character+1):
-    password+=random.choice(latter)
+def cal():
+    character_lower=string.ascii_lowercase
+    character_upper=string.ascii_uppercase
+    Digits=string.digits
+    special_character=string.punctuation
+    store=[] 
+    store.extend(list(character_lower))
+    store.extend(list(character_upper))
+    store.extend(list(Digits))
+    store.extend(list(special_character))
+    random.shuffle(store)
+    password="".join(store[0:user_len])
+    print("Your password is:",password)
     
-for num in range(1,size_of_number+1):
-    password+=random.choice(number)
-    
-for sep in range(1,size_of_special+1):
-    password+=random.choice(special_character)
-    
-for strong in range(1,len(password)):
-    strong_password+=random.choice(password)
-    
-print('The password is :',password)
-print("The Strong password is:",strong_password)
-
+try:
+    user_len=int(input("Enter Password Length: \n"))
+    if(user_len!=str):
+            cal()
+except ValueError:
+    print("Please enter a valid number")
